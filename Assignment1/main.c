cc748a1dc2319f2f16333b0a3796869815b57ceb
Dylan Gray <dg@pcp354022pcs.unl.edu>
2015-08-24 14:09:39 -0500

    Initial Commit

---
+++ Assignment1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment1
+//
+//  Created by Dylan Gray on 8/24/15.
+//  Copyright (c) 2015 Dylan Gray. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
