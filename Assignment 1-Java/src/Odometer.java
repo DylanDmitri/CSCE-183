8c85cc177278d5f553b99f4bfb7f14a963a59f4a
Lee Fitchett <lfitchett@gmail.com>
2015-08-26 10:39:13 -0500

    Fixed odometer

--- Assignment 1-Java/src/Odometer.java
+++ Assignment 1-Java/src/Odometer.java
@@ -10,28 +10,32 @@ import java.util.*;
 public class Odometer {
 	public static void main(String[] args) {
 
-		Scanner console = new Scanner(System.in);			
-		
+		Scanner console = new Scanner(System.in);
+
 		System.out.print("Odometer Start: ");
 		double odometerStart = console.nextDouble();
-		
+
 		System.out.print("Odometer End: ");
 		double odometerEnd = console.nextDouble();
-	
+
 		System.out.print("Gallons Used: ");
 		double gasUsed = console.nextDouble();
-		
+
 		System.out.print("Gas Price: ");
 		double pricePerGallon = console.nextDouble();
-		
+
 		double milesDriven = odometerEnd - odometerStart;
-		double milesPerGallon = milesDriven / gasUsed; 
+		double milesPerGallon = milesDriven / gasUsed;
 		double costPerMile = pricePerGallon / milesPerGallon;
-		
+
 		System.out.println();
-		System.out.println("Miles Driven: " + milesDriven);
-		System.out.println("Miles per gallon: " + milesPerGallon);
-		System.out.printf("Cost per mile: $%.2f", costPerMile);
+		if (milesDriven > 0) {
+			System.out.println("Miles Driven: " + milesDriven);
+			System.out.println("Miles per gallon: " + milesPerGallon);
+			System.out.printf("Cost per mile: $%.2f", costPerMile);
+		} else {
+			System.out.println("Invalid Milage");
+		}
 
 		console.close();
 	}
